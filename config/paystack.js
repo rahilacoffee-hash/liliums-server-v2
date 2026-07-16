c78875f7c1d1b23d1961649df1604aafa3f8d95c
fred <abrahamfred123@gmail.com>
2026-07-16 21:02:54 +0100

    add fixs

--- config/paystack.js
+++ config/paystack.js
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios from "axios";
 
 const paystack = axios.create({
   baseURL: "https://api.paystack.co",
@@ -6,21 +6,31 @@ const paystack = axios.create({
     Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
     "Content-Type": "application/json",
   },
-})
+  timeout: 30000,
+});
 
-export async function initializeTransaction({ email, amountInKobo, metadata, callback_url }) {
-  const response = await paystackApi.post("/transaction/initialize", {
+export async function initializeTransaction({
+  email,
+  amountInKobo,
+  metadata,
+  callback_url,
+}) {
+  const response = await paystack.post("/transaction/initialize", {
     email,
     amount: amountInKobo,
     metadata,
     callback_url,
-  })
-  return response.data
+  });
+
+  return response.data;
 }
 
 export async function verifyTransaction(reference) {
-  const response = await paystackApi.get(`/transaction/verify/${reference}`)
-  return response.data
+  const response = await paystack.get(
+    `/transaction/verify/${reference}`
+  );
+
+  return response.data;
 }
 
-export default paystack
+export default paystack;
